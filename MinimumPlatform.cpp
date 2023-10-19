5246d73ece56dd254ce94f38c1561314559886fa
sahilyadav-1 <121487105+sahilyadav-1@users.noreply.github.com>
2023-10-20 01:35:06 +0530

    Update MinimumPlatform.cpp

--- MinimumPlatform.cpp
+++ MinimumPlatform.cpp
@@ -1,3 +1,33 @@
+APPROACH 1 -
+
+int findPlatform(int arr[], int dep[], int n)
+{
+    vector<pair<int, int> > arr2(n);
+ 
+    for (int i = 0; i < n; i++) {
+        arr2[i] = { arr[i], dep[i] };
+    }
+
+    sort(arr2.begin(), arr2.end());
+ 
+    priority_queue<int, vector<int>, greater<int> > p;
+    int count = 1;
+    p.push(arr2[0].second);
+ 
+    for (int i = 1; i < n; i++) {
+        if (p.top() >= arr2[i].first) {
+            count++;
+        }
+        else {
+            p.pop();
+        }
+        p.push(arr2[i].second);
+    }
+    return count;
+}
+
+APPROACH 2 -
+
 int minimum_platform(int a[],int d[],int n){
     sort(a,a+n);
     sort(d,d+n);
